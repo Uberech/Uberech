e449a2ef7788fc7e8641b38245777227b1dab42b
ldco2016 <renaissance.scholar2012@gmail.com>
2016-03-11 19:35:29 +0000

    Added about page, routes and modified pages_spec

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.feature "Visiting Pages" do
 
   scenario "Home"  do
+    visit "/"
   end
 
   scenario "About"  do
+    visit "/about"
   end
 
   scenario "Products"  do
+    visit "/products"
   end
 end
